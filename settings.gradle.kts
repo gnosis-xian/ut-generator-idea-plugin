7f860572ff9fc5f582028b94e3f0614f84ee7e1e
GitHub Action <action@github.com>
2023-05-24 14:54:45 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "ut-generator-idea-plugin"
--- src/main/kotlin/com/github/gnosisxian/utgeneratorideaplugin/MyBundle.kt
+++ src/main/kotlin/com/github/gnosisxian/utgeneratorideaplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.gnosisxian.utgeneratorideaplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
